2167fea6eeb74497b634c91511d066bcdae6d784
Joseph Hewitt <joseph@jhewitt.net>
2023-06-02 21:36:38 +0200

    Bump version number
    1.1.0rc1 -> 1.1.0

--- A/A.ino
+++ A/A.ino
@@ -1,7 +1,7 @@
 //Joseph Hewitt 2023
 //This code is for the ESP32 "Side A" of the wardriver hardware revision 3.
 
-const String VERSION = "1.1.0rc1";
+const String VERSION = "1.1.0";
 
 #include <GParser.h>
 #include <MicroNMEA.h>
